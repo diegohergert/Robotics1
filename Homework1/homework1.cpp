65c044418744e0e71bb1a7d30e8f0773a55efe9b
Diego Hergert <deh63370@uga.edu>
2025-04-02 13:45:27 -0400

    pls

--- Homework1/homework1.cpp
+++ Homework1/homework1.cpp
@@ -1,9 +1,21 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/features2d.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include <iostream>
-#include <vector>
-
 
 int main() {
-    
+    std::string imagePath = "Homework1/200_images/sample.jpg"; // Pick any test image
+    cv::Mat image = cv::imread(imagePath);
+
+    if (image.empty()) {
+        std::cerr << "Failed to load image at " << imagePath << std::endl;
+        return -1;
+    }
+
+    std::cout << "Image loaded successfully! Size: " 
+              << image.cols << "x" << image.rows << std::endl;
+
+    cv::imshow("Test Image", image);
+    cv::waitKey(0);
+    return 0;
 }
