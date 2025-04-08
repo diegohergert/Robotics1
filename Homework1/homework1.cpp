f662f73f1bcd2ed29c42a7995206dd6d5a951ad7
Diego Hergert <deh63370@uga.edu>
2025-04-08 13:38:24 -0400

    Have two images side by side to start tracking

--- Homework1/homework1.cpp
+++ Homework1/homework1.cpp
@@ -1,24 +1,38 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgcodecs/imgcodecs.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 
 int main() {
-    std::cout << "🚀 OpenCV image test started!" << std::endl;
+    std::cout << "OpenCV image test started!" << std::endl;
 
-    std::string imagePath = "200_images/000000.png";  // Make sure this image exists
-    cv::Mat image = cv::imread(imagePath);
+    std::string imagePath1 = "200_images/000000.png";
+    std::string imagePath2 = "200_images/000001.png";
+    cv::Mat image1 = cv::imread(imagePath1);
+    cv::Mat image2 = cv::imread(imagePath2);
 
-    if (image.empty()) {
-        std::cerr << "❌ Failed to load image at " << imagePath << std::endl;
+    if (image1.empty() || image2.empty()) {
+        std::cerr << "Failed to load images" << std::endl;
         system("pause");
         return -1;
     }
 
-    std::cout << "✅ Image loaded! Size: " << image.cols << " x " << image.rows << std::endl;
+    cv::Mat resimage1, resimage2;
+    cv::resize(image1, resimage1, cv::Size(760, 480), 0, 0);
+    cv::resize(image2, resimage2, cv::Size(760, 480), 0, 0);
 
-    cv::imshow("Test Image", image);
-    cv::waitKey(0);
+
+
+    
+    std::cout << "Image1 loaded! Size: " << resimage1.cols << " x " << resimage1.rows << std::endl;
+    std::cout << "Image1 loaded! Size: " << resimage2.cols << " x " << resimage2.rows << std::endl;
+
+    cv::Mat combined;
+    cv::hconcat(resimage1, resimage2, combined);
+    cv::imshow("Previous (Left) + Current (Right)", combined);
+    cv::waitKey
+    (0);
 
     return 0;
 }
