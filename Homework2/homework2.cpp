6ea1c14484bfcab2cc851288a73d81a75852e4e6
Diego Hergert <deh63370@uga.edu>
2025-04-27 18:31:57 -0400

    progress

--- Homework2/homework2.cpp
+++ Homework2/homework2.cpp
@@ -1,6 +1,28 @@
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Program started." << std::endl;
+    // Set your image path here
+    std::string imagePath = "C:/Users/diego/Downloads/dataset/sequences/00/image_2/000000.png";
+    std::cout << "Trying to load image: " << imagePath << std::endl;
+    // Load the image
+    cv::Mat image = cv::imread(imagePath);
+
+    // Check if the image is loaded successfully
+    if (image.empty()) {
+        std::cerr << "Error: Could not open or find the image at " << imagePath << std::endl;
+        return -1;
+    }
+
+    // Create a window
+    cv::namedWindow("Test Image", cv::WINDOW_NORMAL);
+
+    // Show the image
+    cv::imshow("Test Image", image);
+
+    // Wait for a key press indefinitely
+    cv::waitKey(0);
+
     return 0;
 }
